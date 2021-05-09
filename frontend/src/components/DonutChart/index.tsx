0ccae27b0be61d6670f8b429648f36a71de16eb1
Ana <anamelem4@gmail.com>
2021-05-09 20:24:41 -0300

    .

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
@@ -8,14 +10,21 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    //forma errada
-    let chartData: ChartData = {labels:[], series:[]};
-    //forma errada
-
+    const [chartData, setChartData] = useState<ChartData>({labels:[], series:[]}); 
+    
+    useEffect(()=>{
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-             .then (response => {
-                console.log(response.data);
-                 });
+        .then(response => {
+            const data = response.data as SaleSum[]; 
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            setChartData ({labels:myLabels, series:mySeries});
+           console.log(response.data);
+            });
+    },)
+
+       
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
